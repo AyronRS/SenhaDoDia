1e28321791257620d28d148fdd4b700756541c18
AyronRS <94002301+AyronRS@users.noreply.github.com>
2026-01-19 17:06:24 -0300

    Removido chamada de update

--- src/main.py
+++ src/main.py
@@ -44,10 +44,6 @@ PAD_Y = 2
 PAD_BTN_Y = 8   # espaço vertical entre botões
 PAD_BTN_X = 0   # se quiser espaço lateral extra
 
-#Update
-#import update
-#update.check_for_update(CURRENT_VERSION)
-
 
 def ui(callable_):
     """Executa algo com segurança no thread do Tk."""
